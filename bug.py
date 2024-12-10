75bafef9884786a8b9f383ce9472df2c70175f1c
BugHunter-X <askzenai@gmail.com>
2024-12-10 13:30:05 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,10 @@
+def function_with_uncommon_error(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return a / b
+
+result = function_with_uncommon_error(0, 0)
+print(result) # This will give a ZeroDivisionError instead of returning 0
